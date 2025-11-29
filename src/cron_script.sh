bdd3ef271bad71f163d471773208af915eb70fca
smd67 <dimigstephenm@gmail.com>
2025-11-29 06:28:38 -0500

    fixes

--- src/cron_script.sh
+++ src/cron_script.sh
@@ -3,10 +3,8 @@
 # Source pyenv and activate the desired virtual environment
 export PYENV_ROOT="/home/ec2-user/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 
- pyenv local 3.12.11
+pyenv local 3.12.11
 
 # Change to your script's directory (optional, but often good practice)
 cd /home/ec2-user/repos/saa_grafana
