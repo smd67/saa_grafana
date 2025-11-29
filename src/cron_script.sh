8fac5c3805011f4073bbb3614401e38dc63846c3
smd67 <dimigstephenm@gmail.com>
2025-11-29 06:25:33 -0500

    fix

---
+++ src/cron_script.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+# Source pyenv and activate the desired virtual environment
+export PYENV_ROOT="/home/ec2-user/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+ pyenv local 3.12.11
+
+# Change to your script's directory (optional, but often good practice)
+cd /home/ec2-user/repos/saa_grafana
+
+# Execute your Python script
+poetry run  python src/analyze.py --batch --influx >> /path/to/logfile.log 2>&1
